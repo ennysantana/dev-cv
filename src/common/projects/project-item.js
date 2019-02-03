3398267c268b866b21f7f165831b3684bda10d9d
Eyal Perry <eyalpe@wix.com>
2019-02-03 16:07:35 +0200

    better design

--- src/common/projects/project-item.js
+++ src/common/projects/project-item.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './project-item.scss';
-import {Paragraphs, Label} from "Common";
+import { Paragraphs, Label } from "Common";
 
 export class ProjectItem extends React.Component {
     static defaultProps = {
         className: 'project',
-        classNames:{
-            name:'name',
-            description:"description",
-            features:"features",
-            featuresLabel:"features-label"
+        classNames: {
+            name: 'name',
+            description: "description",
+            featuresContainer: "features-container",
+            features: "features",
+            featuresLabel: "features-label"
         },
-        defaults:{
-            featuresLabel:'Features'
+        defaults: {
+            featuresLabel: 'Features'
         }
     };
 
-    static getKey = (item) =>{
+    static getKey = (item) => {
         return item.name;
     };
 
@@ -29,19 +30,21 @@ export class ProjectItem extends React.Component {
             classNames
         } = this.props;
 
-        const {name,
+        const { name,
             description,
             features,
             featuresLabel
-        } = {...defaults, ...value};
+        } = { ...defaults, ...value };
 
         return (
-          <div className={className}>
-              <div className={classNames.name}>{name}</div>
-              <Paragraphs value={description} className={classNames.description}/>
-              <Label content={featuresLabel} className={classNames.featuresLabel}/>
-              <Paragraphs value={features} className={classNames.features}/>
-          </div>
+            <div className={className}>
+                <div className={classNames.name}>{name}</div>
+                <Paragraphs value={description} className={classNames.description} />
+                <div className={classNames.featuresContainer}>
+                    <Label content={featuresLabel} className={classNames.featuresLabel} />
+                    <Paragraphs value={features} className={classNames.features} />
+                </div>
+            </div>
         );
     }
 }
